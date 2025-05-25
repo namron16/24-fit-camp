28cedd9235c24e064bf35efbd3579cb34b4be38e
namron16 <namronpontillo@gmail.com>
2025-05-25 14:43:04 +0800

    fixed admin pages loading usePageTransition delay

--- src/pages/Admin/Members.jsx
+++ src/pages/Admin/Members.jsx
@@ -12,7 +12,7 @@ import "./members.css";
 const Members = () => {
   const { deleteMember } = useDeleteMember();
   const { members } = useFetchMembers();
-  const { isPending, showContent } = usePageTransition(1000);
+  const { isPending, showContent } = usePageTransition(300);
 
   const handleDelete = useCallback(
     (id) => {
